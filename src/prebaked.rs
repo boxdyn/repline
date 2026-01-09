e8cd46852c8daeca815843133deda8ddda8759ff
John <j@soft.fish>
2026-01-09 18:57:58 -0500

    prebaked: add an "advanced" prebaked `read_and_mut(..., |Repline, &str|)`

--- src/prebaked.rs
+++ src/prebaked.rs
@@ -3,7 +3,7 @@
 //! Constructs a [Repline] and repeatedly runs the provided closure on the input strings,
 //! obeying the closure's [Response].
 
-use std::error::Error;
+use std::{error::Error, io::Stdin};
 
 use crate::{error::Error as RlError, repline::Repline};
 
@@ -32,6 +32,23 @@ pub enum Response {
 /// - `Ctrl+D` clears the input, but *runs the closure* with the old input
 pub fn read_and<F>(color: &str, begin: &str, again: &str, mut f: F) -> Result<(), RlError>
 where F: FnMut(&str) -> Result<Response, Box<dyn Error>> {
+    read_and_mut(color, begin, again, |_, line| f(line))
+}
+
+/// Implements a basic menu loop using an embedded [Repline],
+/// provided to the caller's closure, `f`.
+///
+/// Repeatedly runs the provided closure on the input strings,
+/// obeying the closure's [Response]. The closure may modify the
+/// state of the Repline, including taking additional input.
+///
+/// Captures and displays all user [Error]s.
+///
+/// # Keybinds
+/// - `Ctrl+C` exits the loop
+/// - `Ctrl+D` clears the input, but *runs the closure* with the old input
+pub fn read_and_mut<F>(color: &str, begin: &str, again: &str, mut f: F) -> Result<(), RlError>
+where F: FnMut(&mut Repline<'_, Stdin>, &str) -> Result<Response, Box<dyn Error>> {
     let mut rl = Repline::new(color, begin, again);
     loop {
         let line = match rl.read() {
@@ -44,12 +61,12 @@ where F: FnMut(&str) -> Result<Response, Box<dyn Error>> {
             Err(e) => Err(e)?,
         };
         print!("\x1b[G\x1b[J");
-        match f(&line) {
+        match f(&mut rl, &line) {
             Ok(Response::Accept) => rl.accept(),
             Ok(Response::Deny) => rl.deny(),
             Ok(Response::Break) => break,
             Ok(Response::Continue) => continue,
-            Err(e) => rl.print_inline(format_args!("\x1b[40G\x1b[91m{e}\x1b[0m"))?,
+            Err(e) => rl.print_inline(format_args!("    \x1b[91m{e}\x1b[0m"))?,
         }
     }
     Ok(())
